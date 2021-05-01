d5fade1ad92eb3411c58a20ab138c46b4b06fcc1
bunny <yatharthrajput009@gmail.com>
2021-05-01 17:15:38 +0530

    first commit

--- sketch.js
+++ sketch.js
@@ -15,9 +15,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
+     
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -60,5 +60,4 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
 }
